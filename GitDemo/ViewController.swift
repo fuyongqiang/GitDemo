14db36caa0fab94e68d58d3f8585cf32dd9ca3ed
yqfu <yqfu@ipowertec.com>
2015-12-16 09:39:33 +0800

    commit after LastBranch merge into

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -23,6 +23,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        increaseSum()
+        sayHello()
     }
 
     override func didReceiveMemoryWarning() {
